0ed9646c40d618054419036f2edbd26d07f3ffc7
sadiq khan <saiqkhan7777@gmail.com>
2024-11-28 16:27:00 +0500

    html-css-project

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+// Q1) ALERT YOUR NAME
+alert("Sadiq Khan alert box");
+
+// Q2) BY USING NUMBER, STRING, BOOLEAN DATATYPE MAKE A VARIABLES
+let age = 25;              // Number
+let name1 = "Sadiq Khan";   // String
+let isStudent = true;      // Boolean
+console.log(age, name1, isStudent);
+
+// Q3) BY USING BIGINT DATATYPE TYPE THIS NUMBER 0922992929292992
+
+let bigNumber = 0n + 922992929292992n; 
+console.log(bigNumber);
+
+// Q4) BY USING THE PROMPT PLEASE PRINT THE USER'S AGE
+
+//  Confirm Box
+window.confirm("Are you sure? Confirm box");
+
+// Prompt Box
+window.prompt("Please enter your age:Prompt box ");
