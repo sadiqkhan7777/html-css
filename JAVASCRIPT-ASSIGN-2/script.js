1a83b9ca544bc1c16b6aea1f8c08e063570612d9
sadiqkhan7777 <saiqkhansaiqkhan7777@gmail.com>
2025-02-21 22:02:36 +0500

    Update-html-css

---
+++ JAVASCRIPT-ASSIGN-2/script.js
@@ -0,0 +1,104 @@
+                                                                    //  1.Playing with Types
+
+// sting method
+// let myName = "Yamin khan";
+// console.log(myName);
+// console.log("Name:", myName, "Type:", typeof myName);
+
+// number method
+// let age = 26;
+// console.log("Age:", age, "Type:", typeof age);
+
+// boolean method
+// let isStudent = true;
+// console.log("Is StudentL", isStudent, "Type:", typeof isStudent);
+
+// Null method
+// let carModel = null; 
+// console.log("Car Model:", carModel, "Type:", typeof carModel)
+
+// Undefined method
+// let studentName;
+// console.log("Student Name:", studentName, "Type:", typeof studentName)
+
+// Biginit method
+// let bigNumber = BigInt(845875421169854);
+// console.log("Big Number:", bigNumber, "Type:", typeof bigNumber)
+
+// Symbol method
+// let id = Symbol("Name");
+// console.log("ID:", id, "Type:", typeof id)
+
+
+                                                                        //  2. Playing with Variables
+
+// var vacancy = "Frontend Developers";
+// vacancy = "Backend Developers";
+// console.log(vacancy);
+
+// let city = "Karachi";
+// city = "Lahore";
+// console.log(city)
+
+// const language = "JavaScript";
+// language = "Typescript"; // will throw an error because the const can't be changed 
+// console.log(language)
+
+                                                                        //  3. Arrays and Methods
+
+// Accessing the elements of an array
+// let studentName = ["Junaid", "Ali", "Qasim", "Hina", "Hira"];  // i use the studentName array downbelow
+// console.log("Fisrt Name:", studentName[0])
+// console.log("last Name:", studentName[studentName.length - 1])
+
+// Adding the last elements of an a Array
+// studentName.push("Kalim")
+// console.log("After using the push:", studentName)
+
+// Removing the last element of an Array
+// studentName.pop();
+// console.log("After using the pop", studentName)
+
+// Adding the Element of an Array on the start
+// studentName.unshift("Shahzad");
+// console.log("After using the unshift", studentName)
+
+// Removing the first element of an Array
+// studentName.shift();
+// console.log("After shift:", studentName)
+
+// using the other method of an Arrat
+// let sortedName1 = studentName.sort();
+// console.log("Sorted Array:", sortedName1);
+
+// let reversedName = studentName.reverse();
+// console.log("Reversed Array:", reversedName)
+
+// Joining the elements of an Array
+// let joinName = ["Jasir", "Mateen", "Mehmood",]
+// console.log("Joined Array:", joinName.join(" ** "))
+
+
+                                                                           //  4. Loop
+
+// For Loop
+// for (let i = 1; i <= 300; i++){
+//     console.log(i);
+// }
+
+
+// For of loop
+// i'm using the array in loops from line No.51
+// for (let name of studentName){
+//     console.log(name)
+// }
+
+// for in loop
+// let carModel = {
+//     name: "SupraMk4",
+//     model: 2002,
+//     uniqueid: 5418,
+// }
+// for (let key in carModel){
+//     console.log(`${key} : ${carModel[key]}`);
+// }
